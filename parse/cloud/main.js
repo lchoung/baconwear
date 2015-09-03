1d8bcf675b2a4afed89765c91421f5f13447deb2
Ivan Wang <icw@cmu.edu>
2015-09-03 14:45:27 -0400

    set up Cloud code for saving to spreadsheet (WIP)

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -1,8 +1,19 @@
-
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
+/**
+ * Save borrower data to Google Spreadsheet
+ * Spreadsheet should have the format:
+ *
+ *     Label   Borrower  Borrow Date  Return Date         Status
+ * ex  wla0014  Nancy    09/03/2015   09/10/2015  Pending|Loaned|Returned
+ */
 Parse.Cloud.afterSave('Clothing', function(request) {
-  // TODO: use request.object and add field to google spreadsheet
-  // if label already exists, then update that row
-  console.log(request.object);
+  var label = request.object.label;
+  var borrower = request.object.borrower;
+  var borrowDate = requiest.object.borrowDate;
+  var returnDate = request.object.returnDate;
+
+  if (/*label exists in col A of spreadsheet*/ true) {
+    // TODO: update that row's borrower data
+  } else if (borrower) {
+    // TODO: create row with new data and status 'Pending'
+  }
 });
