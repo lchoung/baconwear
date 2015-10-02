9249748bb40082f7c3794888d6b59346f697f48a
Ivan Wang <icw@cmu.edu>
2015-10-02 13:56:55 -0400

    update form with available sizes

--- js/BorrowForm.react.js
+++ js/BorrowForm.react.js
@@ -1,6 +1,8 @@
 var DatePicker = require('react-datepicker');
 var ParseReact = require('parse-react');
 var React = require('react');
+
+var cx = require('classnames');
 var moment = require('moment');
 
 var DATE_FORMAT = 'ddd MMM Do, YYYY';
@@ -8,6 +10,7 @@ var DATE_FORMAT = 'ddd MMM Do, YYYY';
 var BorrowForm = React.createClass({
   propTypes: {
     name: React.PropTypes.string,
+    label: React.PropTypes.string,
     photos: React.PropTypes.shape({
       main: React.PropTypes.object,
       large: React.PropTypes.object,
@@ -15,9 +18,9 @@ var BorrowForm = React.createClass({
     gender: React.PropTypes.string,
     status: React.PropTypes.string,
     style: React.PropTypes.string,
-    borrower: React.PropTypes.string,
+    /*borrower: React.PropTypes.string,
     borrowDate: React.PropTypes.object,
-    returnDate: React.PropTypes.object,
+    returnDate: React.PropTypes.object,*/
 
     onCancel: React.PropTypes.func.isRequired,
     onSuccess: React.PropTypes.func.isRequired,
@@ -29,6 +32,7 @@ var BorrowForm = React.createClass({
       name: '',
       andrewID: '',
       returnDate: null,
+      size: '',
       submitting: false,
     };
   },
@@ -40,14 +44,14 @@ var BorrowForm = React.createClass({
 
   _onSubmit: function() {
     this.setState({submitting: true});
-    var {name, andrewID, returnDate} = this.state;
-    var canSubmit = name && andrewID && returnDate;
+    var {name, andrewID, returnDate, size} = this.state;
+    var canSubmit = name && andrewID && returnDate && size;
     if (!canSubmit) {
       return;
     }
 
     // Mutate Parse borrow data
-    ParseReact.Mutation.Set(this.props.clothing, {
+    /*ParseReact.Mutation.Set(this.props.clothing, {
       borrower: name,
       borrowDate: moment()._d,
       returnDate: returnDate._d,
@@ -55,6 +59,8 @@ var BorrowForm = React.createClass({
     }).dispatch()
     .done(() => this._onSuccess(name, andrewID, returnDate))
     .fail(this.props.onFailure);
+    */
+    console.log('submitted');
   },
 
   _onSuccess: function(name, andrewID, returnDate) {
@@ -67,7 +73,7 @@ var BorrowForm = React.createClass({
       ? this.props.photos.main._url
       : 'http://placekitten.com/180/240';
 
-    var {name, andrewID, returnDate} = this.state;
+    var {name, andrewID, returnDate, size} = this.state;
     var canSubmit = name && andrewID && returnDate;
 
     var closeButton = (
@@ -85,6 +91,17 @@ var BorrowForm = React.createClass({
         Submit
       </button>
     );
+
+    var SIZES = {
+      S: 'Small',
+      M: 'Medium',
+      L: 'Large',
+    };
+    var sizeOptions = ['S', 'M', 'L'].filter((size) =>
+      this.props.quantity[size] > 0
+    ).map((size) => {
+      return <option key={size} value={size}>{SIZES[size]}</option>;
+    });
 
     var form = (
       <form className="form right">
@@ -108,7 +125,18 @@ var BorrowForm = React.createClass({
             value={andrewID}
           />
         </div>
-        <div className="form-group">
+        <div className="form-group form-group-small">
+          <label htmlFor="input-size">Size</label>
+          <select className="form-control width-small" id="input-size"
+            disabled={this.state.submitting}
+            onChange={this.handleChange.bind(this, 'size')}
+            value={size}
+          >
+            {sizeOptions}
+          </select>
+        </div>
+
+        <div className="form-group form-group-small">
           <label htmlFor="input-date">Borrow Until...</label>
           <DatePicker
             className="datepicker__input"
@@ -123,10 +151,19 @@ var BorrowForm = React.createClass({
             onChange={this.handleChange.bind(this, 'returnDate')}
           />
         </div>
-        {closeButton}
-        {submitButton}
+
+        <div className="form-buttons text-right">
+          {closeButton}
+          {submitButton}
+        </div>
       </form>
     );
+
+    var sizeClass = (size) => {
+      return cx('col-lg-4', {
+        'clothing-out': !this.props.quantity[size],
+      });
+    };
 
     return (
       <div className="borrow-form clearfix">
@@ -134,6 +171,12 @@ var BorrowForm = React.createClass({
         <div className="pull-left">
           <h4 className="clothing-title">{this.props.name}</h4>
           <img className="image" width={180} height={240} src={imageSource} />
+        <div className="row clothing-sizes">
+          <span className={sizeClass('S')}>{this.props.quantity.S || 0} &times; S</span>
+          <span className={sizeClass('M')}>{this.props.quantity.M || 0} &times; M</span>
+          <span className={sizeClass('L')}>{this.props.quantity.L || 0} &times; L</span>
+        </div>
+
         </div>
         {form}
       </div>
