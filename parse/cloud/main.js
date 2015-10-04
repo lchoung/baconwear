d3de6c9985591148ea27ea033d696d962549e306
Ivan Wang <icw@cmu.edu>
2015-10-04 13:32:27 -0400

    send email notifications upon new loan creation

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -1,19 +1,63 @@
-/**
- * Save borrower data to Google Spreadsheet
- * Spreadsheet should have the format:
- *
- *     Label   Borrower  Borrow Date  Return Date         Status
- * ex  wla0014  Nancy    09/03/2015   09/10/2015  Pending|Loaned|Returned
- */
-Parse.Cloud.afterSave('Clothing', function(request) {
-  var label = request.object.label;
-  var borrower = request.object.borrower;
-  var borrowDate = requiest.object.borrowDate;
-  var returnDate = request.object.returnDate;
+var Mailgun = require('mailgun');
+var moment = require('moment');
 
-  if (/*label exists in col A of spreadsheet*/ true) {
-    // TODO: update that row's borrower data
-  } else if (borrower) {
-    // TODO: create row with new data and status 'Pending'
+
+// Initialize Mailgun client
+var DOMAIN = 'sandboxa5240d7ba154408cb116da96154c0ee0.mailgun.org';
+var API_KEY = 'key-7280cabdb5a610e1784ad81908001e23';
+Mailgun.initialize(DOMAIN, API_KEY);
+
+var DATE_FORMAT = 'ddd MMM Do, YYYY';
+
+
+/**
+ * Send email notifications upon loan creation using Mailgun API
+ */
+Parse.Cloud.afterSave('Loan', function(request) {
+  // Don't do anything for updates, only new created loans
+  if (request.object.existed()) {
+    return;
   }
+
+  function sendEmail(recipients) {
+    Mailgun.sendEmail({
+      to: recipients.join(', '),
+      from: 'ballroom@andrew.cmu.edu',
+      subject: 'Ballroom Closet loan notification',
+      text: [
+        'Hello! This is a friendly notification that',
+        request.object.get('borrower'),
+        '(' + request.object.get('andrewId') + ')',
+        'has requested a loan of',
+        request.object.get('name'),
+        '(' + request.object.get('label') + ')',
+        'from',
+        moment(request.object.borrowDate).format(DATE_FORMAT),
+        'until',
+        moment(request.object.returnDate).format(DATE_FORMAT),
+        '. Please visit http://www.cmubdc.org/closet/admin to approve or deny this loan.'
+      ].join(' ')
+    }, {
+      success: function(httpResponse) {
+        console.log('successfully sent email notification');
+      },
+      error: function(httpResponse) {
+        console.error('failed to send email!');
+        console.log(httpResponse);
+      }
+    });
+  }
+
+  new Parse.Query('Admin').find({
+    success: function(admins) {
+      var emails = admins.map(function(admin) {
+        return admin.get('email');
+      });
+      sendEmail(emails);
+    },
+    error: function(error) {
+      console.error('failed to retrieve admin emails: ' + error.message);
+    }
+  });
+
 });
